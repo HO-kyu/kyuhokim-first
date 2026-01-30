bd6dd119e7fc130577235441d9311efc7c5146e1
khbkkh0828 <khbkkh0828@gmail.com>
2026-01-30 23:07:38 +0000

    feat: Add dark/light mode toggle

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// Add JS here
+const themeToggle = document.getElementById('checkbox');
+const body = document.body;
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+    body.setAttribute('data-theme', savedTheme);
+    if (savedTheme === 'dark') {
+        themeToggle.checked = true;
+    }
+}
+
+themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+        body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        body.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+});
